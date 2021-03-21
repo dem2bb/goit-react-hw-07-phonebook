0783a3dca22c6c4bcfafd0c35f85cdb5d5f01c9a
Maxim Kazachuk <dem2bb1@gmail.com>
2021-03-21 21:11:44 +0200

    Updates

---
+++ static/js/7.65baaf79.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-02-phonebook"]=this["webpackJsonpgoit-react-hw-02-phonebook"]||[]).push([[7],{114:function(e,o,t){"use strict";t.r(o);t(0);var n=t(1);o.default=function(){return Object(n.jsx)("h1",{children:"HomePage"})}}}]);
+//# sourceMappingURL=7.65baaf79.chunk.js.map
